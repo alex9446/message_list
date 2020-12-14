32b744bb4a1aa4ce3dd57d91767557706ee6a0cc
Alessio Margelli <alex9446@gmail.com>
2020-12-14 23:51:06 +0100

    🗜️ Removed duplicate files which have no jsx code

--- src/js/fetch_functions.js
+++ src/js/fetch_functions.js
@@ -1,5 +1,8 @@
 function general_fetch(url, callback, onError = null, options = {}) {
-  fetch(url, options).then(response => response.json()).then(result => callback(result)).catch(error => {
+  fetch(url, options)
+  .then(response => response.json())
+  .then(result => callback(result))
+  .catch(error => {
     if (onError) {
       onError(error);
     } else {
@@ -14,11 +17,12 @@ function general_fetch_with_options(url, options, callback, onError = null) {
 
 export function fetch_last_action(lastAction, setLastAction) {
   general_fetch('/last_action_on_messages', result => {
-    if (result.value && result.value !== lastAction) {
+    if (result.value && (result.value !== lastAction)) {
       setLastAction(result.value);
     }
   });
 }
+
 export function fetch_messages(setMessages) {
   general_fetch('/messages', result => {
     if (result.error) {
@@ -28,63 +32,74 @@ export function fetch_messages(setMessages) {
     }
   });
 }
+
 export function add_message(text, addError) {
   function handleError(error) {
     console.error(error);
     addError('Message not added');
   }
-
-  general_fetch_with_options('/messages', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
+  general_fetch_with_options(
+    '/messages',
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        text: text
+      })
     },
-    body: JSON.stringify({
-      text: text
-    })
-  }, result => {
-    if (result.error) {
-      handleError(result.error);
-    } else {
-      console.debug('Message created!');
-    }
-  }, handleError);
+    result => {
+      if (result.error) {
+        handleError(result.error);
+      } else {
+        console.debug('Message created!')
+      }
+    },
+    handleError
+  );
 }
+
 export function edit_message(id, text, addError) {
   function handleError(error) {
     console.error(error);
     addError('Message not edited');
   }
-
-  general_fetch_with_options('/messages/' + id, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json'
+  general_fetch_with_options(
+    '/messages/' + id,
+    {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        text: text
+      })
     },
-    body: JSON.stringify({
-      text: text
-    })
-  }, result => {
-    if (result.error) {
-      handleError(result.error);
-    } else {
-      console.debug('Message edited!');
-    }
-  }, handleError);
+    result => {
+      if (result.error) {
+        handleError(result.error);
+      } else {
+        console.debug('Message edited!')
+      }
+    },
+    handleError
+  );
 }
+
 export function delete_message(id, addError) {
   function handleError(error) {
     console.error(error);
     addError('Message not deleted');
   }
-
-  general_fetch_with_options('/messages/' + id, {
-    method: 'DELETE'
-  }, result => {
-    if (result.error) {
-      handleError(result.error);
-    } else {
-      console.debug('Message deleted!');
-    }
-  }, handleError);
+  general_fetch_with_options(
+    '/messages/' + id,
+    {
+      method: 'DELETE'
+    },
+    result => {
+      if (result.error) {
+        handleError(result.error);
+      } else {
+        console.debug('Message deleted!')
+      }
+    },
+    handleError
+  );
 }
