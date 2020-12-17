ed0ab154c95a4e4da3fa3b9b211155d94525f96d
Alessio Margelli <alex9446@gmail.com>
2020-12-17 23:50:39 +0100

    ⬆️ Bumped version to 1.1.1

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
     app: './src/js/app.js',
   },
